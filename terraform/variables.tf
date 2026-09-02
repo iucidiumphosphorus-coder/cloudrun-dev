ba692d16d594eabcc1373da9fbcece7da8c6a57a
iucidiumphosphorus-coder <iucidium.phosphorus@gmail.com>
2026-09-02 16:01:04 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,13 @@
+variable "project_id" {
+  type        = string
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP Region"
+}
+
 variable "allowed_user_email" {
   type        = list(string)
   description = "IAPでアクセスを許可するユーザーのGoogleアカウントのリスト"
